36ce2d7787d1f7d479463e55d2e81651b54610e9
Islam Azab <islam.azab@badrit.com>
2013-01-13 21:27:31 +0200

    Created clean game project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Game::Application.config.secret_token = 'a144d5359a215fbac637d8c8574959aa415d2ed1742d754529f533378396990713a19eda83b43fd7d8133eb37115eb2193867d5d39e5e2af56319defc2e73c37'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Game::Application.routes.draw do
+  root :to => "home#index"
+end
