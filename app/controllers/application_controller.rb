e0901db9420f8545316327e40400395346f282f5
Islam Azab <islam.azab@badrit.com>
2013-01-16 14:08:15 +0200

    Added logout working link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  def after_sign_up_path_for(resource) 
+
+		"www.google.com" 
+
+	end
 end
