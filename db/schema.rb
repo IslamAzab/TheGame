68cc7639beaaa4837aefc242ca3d694652b7c9f1
Islam Azab <islam.azab@badrit.com>
2013-01-16 13:31:18 +0200

    Add username column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130113201250) do
+ActiveRecord::Schema.define(:version => 20130116112948) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130113201250) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
