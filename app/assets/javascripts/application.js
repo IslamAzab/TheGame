e1bbf8b0a1af7f819fceb7419f3aaa4d04cf09a4
Islam Azab <islam.azab@badrit.com>
2013-01-22 20:39:19 +0200

    changed remove_scoring_cards from checkbox to link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,11 @@
 //= require bootstrap
 //= require_tree .
 
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).parent().parent().hide();
+}
+
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g");
