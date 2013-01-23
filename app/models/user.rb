b635ca89eb79902d461ffa00d62793abb7fb14a2
Islam Azab <islam.azab@badrit.com>
2013-01-23 20:51:56 +0200

    added small thumbnails for users listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   attr_accessible :login, :username, :first_name, :last_name, :title
 
   attr_accessible :avatar
-  has_attached_file :avatar,:styles =>{ :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar,:styles =>{ :medium => "300x300>", :thumb => "32x32>" }
 
   # Validation
   validates :username, :presence => true, :uniqueness => {:case_sensitive => false}
