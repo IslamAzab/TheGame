6c2b4280d9668d8a95324ada8caf4170f2685c6d
ELbayaa <ahmed.elbaiaa@badrit.com>
2013-02-25 00:45:53 +0200

    date format handled

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -42,8 +42,8 @@ function displayResult(){
   var start_date = $("#from_date").val();
   var end_date = $("#to_date").val();
   
-  // swap the dates if end > start
-  if (end_date < start_date){
+  // swap the dates if end < start
+  if (isSmaller(end_date, start_date)){
     var temp = start_date;
     start_date = end_date;
     end_date = temp;
@@ -61,4 +61,14 @@ function displayResult(){
     },
     dataType: 'script'
   });
+}
+
+/**
+compares two date and return true if start date < end date where the date format is dd-mm-yyyy
+*/
+function isSmaller(start, end){
+  var sParts = start.split("-");
+  var eParts = end.split("-");
+  var result = sParts[2] == eParts[2] ? sParts[1] == eParts[1] ? sParts[0] == eParts[0] ? false : sParts[0] < eParts[0] : sParts[1] < eParts[1]  : sParts[2] < eParts[2] 
+  return result
 }
--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -4,13 +4,13 @@ module GameHelper
     today = Date.today
 
     [ 
-      ["Today",{:'data-start_day' => l(today), :'data-end_day' => l(today)}],
+      ["Today",{'data-start_day' => l(today), 'data-end_day' => l(today)}],
 
-      ["This week",{:'data-start_day' => l(today.beginning_of_week(start_day = :sunday)),
-             :'data-end_day' => l(today.end_of_week(start_day = :sunday))}],
+      ["This week",{'data-start_day' => l(today.beginning_of_week(start_day = :sunday)),
+             'data-end_day' => l(today.end_of_week(start_day = :sunday))}],
       
-      ["This month",{:'data-start_day' => l(today.beginning_of_month),
-             :'data-end_day' => l(today.end_of_month)}]
+      ["This month",{'data-start_day' => l(today.beginning_of_month),
+             'data-end_day' => l(today.end_of_month)}]
     ]
   end
 end
