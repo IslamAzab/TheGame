8cc88c858be3ea00f14382ed2099fbc762442ec9
Ahmed ElBayaa <ahmed.elbaiaa@badrit.com>
2013-03-25 18:58:08 +0200

    Do not delete scoring cards with results

--- app/models/scoring_card.rb
+++ app/models/scoring_card.rb
@@ -1,5 +1,11 @@
 class ScoringCard < ActiveRecord::Base
+  
   belongs_to :user
+  
+  # don't destroy scoring card if player played with it
+  # this before filter should be declared before has_many: results
+  # to call its method "ensure_not_played" before destroying results
+  before_destroy :ensure_not_played
   has_many :results, :dependent => :nullify
 
   attr_accessible :active, :score, :title, :avatar
@@ -11,6 +17,16 @@ class ScoringCard < ActiveRecord::Base
   validates :score, :presence => true, :numericality => true
   validates :title, :presence => true
 
+
   scope :active_cards, where(:active => true)
   scope :inactive_cards, where(:active => false)
+
+  def ensure_not_played
+    if self.results.any?
+      self.errors.add(:base, "Cannot delete #{self.title}, #{self.user.full_name} already played with it.")
+      return false
+    else
+      return true
+    end
+  end
 end
