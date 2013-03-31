fb8fdad352822e200d1385dedab5c986f5b3bc4b
Ahmed ElBayaa <ahmed.elbaiaa@badrit.com>
2013-03-31 11:57:46 +0200

    change default from

--- app/mailers/result_notifier.rb
+++ app/mailers/result_notifier.rb
@@ -1,5 +1,5 @@
 class ResultNotifier < ActionMailer::Base
-  default from: "'hrs' <email@example.com>"
+  default from: "'hrs' <hrs@badrit.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
